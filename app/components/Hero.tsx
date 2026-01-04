0b6858058579b409cc324063bf3f6464493ff7ad
Renggars <renggarendy22@gmail.com>
2026-01-04 20:27:45 +0700

    feat(ui): make hero section responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,40 +2,36 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
-  const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
-    <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
+    <div className="relative flex xl:flex-row flex-col gap-5 z-0 w-full ">
+      <div className="flex-1 pt-24 xl:pt-36 px-6 sm:px-14  max-w-[1440px] lg:max-w-2xl xl:pr-0">
+        <h1 className="2xl:text-[72px] sm:text-[64px] lg:max-w-2xl text-4xl font-extrabold text-slate-900 leading-[1.4] sm:leading-[1.2] lg:leading-[1.1]">
           Cari, Pesan, atau Sewa Mobil — Cepat dan Tanpa Ribet!
         </h1>
 
-        <p className="text-2xl">
+        <p className="text-lg sm:text-xl lg:text-2xl text-slate-600 font-light mt-2 leading-relaxed">
           Permudah pengalaman sewa mobil Anda dengan proses pemesanan yang
           praktis. Rentcar menyediakan berbagai pilihan armada untuk segala
           kebutuhan perjalanan Anda.
         </p>
 
-        <a
-          href={"#armada"}
-          className="mt-10 px-8 py-3 bg-primary-blue text-white font-semibold rounded-full shadow-lg 
-                     hover:bg-blue-700 hover:shadow-primary-blue/30 active:scale-95 transition-all"
-        >
-          Lihat Armada
-        </a>
+        <div className="mt-5 ">
+          <Link
+            href="#armada"
+            className="inline-block px-8 py-4 bg-primary-blue text-white font-bold rounded-full shadow-lg shadow-blue-200 hover:bg-blue-700 active:scale-95 transition-all text-[16px] z-10"
+          >
+            Lihat Armada
+          </Link>
+        </div>
       </div>
 
-      <div className="hero__image-container">
-        <div className="hero__image">
+      {/* Kanan */}
+      <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+        <div className="relative w-[70%] xl:w-[90%] xl:h-full h-[200px] sm:h-[450px] md:h-[590px] -z-10 -top-20 sm:-top-52 md:-top-48 xl:top-0">
+          {/* Mobil Alphard */}
           <Image
             src="/images/toyota-alphard.png"
             alt="Rentcar - Sewa Mobil Toyota Alphard"
@@ -45,7 +41,12 @@ const Hero = () => {
           />
         </div>
 
-        <div className="hero__image-overlay"></div>
+        <div
+          className="absolute 
+            top-[55%] sm:top-[30%] xl:-top-10 -right-[40%] sm:-right-1/2 md:-right-[40%]  xl:-right-1/2             bg-hero-bg bg-repeat-round -z-20 
+            w-full h-[250px] sm:h-[500px] md:h-[550px] xl:h-screen
+            overflow-hidden"
+        />
       </div>
     </div>
   );
